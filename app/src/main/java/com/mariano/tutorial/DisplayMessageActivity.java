bdf072b2af1f8e76b19cd14307097f0c5c7bb216
Mariano Sabatucci <marosaba83@gmail.com>
2015-09-06 17:20:17 -0300

    Starting Another Activity  - Completed
    done!

--- app/src/main/java/com/mariano/tutorial/DisplayMessageActivity.java
+++ app/src/main/java/com/mariano/tutorial/DisplayMessageActivity.java
@@ -5,17 +5,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
-    public final static String EXTRA_MESSAGE = "com.mycompany.tutorial.MESSAGE";
+public class DisplayMessageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_display_message);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+        setContentView(textView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_display_message, menu);
+        return true;
     }
 
     @Override
@@ -31,14 +43,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void sendMessage (View view) {
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
     }
 }
